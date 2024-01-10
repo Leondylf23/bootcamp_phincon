36ed206532eeb945555ea1fff3ae7563be359f8e
Leondy <54894350+Leondylf23@users.noreply.github.com>
2024-01-10 16:32:39 +0700

    update refactor

--- Day3/script.js
+++ Day3/script.js
@@ -16,8 +16,8 @@ function toggleMenu(){
     }
 }
 
-var carouselIndex = 0;
 const carouselImgs = ["./imgs/content-2-img.jpg", "./imgs/bg1.png", "./imgs/bg2.png", "./imgs/bg3.png", "./imgs/bg4.png"];
+var carouselIndex = 0;
 function carouselLeft() {
     let carouselRadios = document.getElementById("carouselRadios").children;
     let carouselContent = document.getElementById("carouselContent");
@@ -44,3 +44,27 @@ function carouselRight() {
     carouselRadios[carouselIndex].className = "radio active";
     carouselContent.src = carouselImgs[carouselIndex];
 }
+function radioClick(index) {
+    let carouselRadios = document.getElementById("carouselRadios").children;
+    let carouselContent = document.getElementById("carouselContent");
+    
+    carouselRadios[carouselIndex].className = "radio";
+    carouselIndex = index;
+    carouselRadios[index].className = "radio active";
+    carouselContent.src = carouselImgs[index];
+}
+function initImg() {
+    let carouselContent = document.getElementById("carouselContent");
+    const carouselRadios = document.getElementById("carouselRadios");
+    for (let i = 0; i < carouselImgs.length; i++) {
+        let radio = document.createElement("div");
+        radio.className = i === 0 ? "radio active" : "radio";
+        radio.addEventListener("click", () => radioClick(i));
+        carouselRadios.appendChild(radio);
+    }
+    if(carouselImgs.length > 0) {
+        carouselContent.src = carouselImgs[0];
+    }
+}
+
+initImg();
